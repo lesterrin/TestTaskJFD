84d202acb616d6cb48e3dce35241a6b5b5c37aab
VS <voldemar1810@gmail.com>
2023-04-29 00:09:32 +1000

    Исправлен баг, при котором таблица не обновлялась при стирании поля ввода до <3 символов

--- practice/3/scripts.js
+++ practice/3/scripts.js
@@ -14,9 +14,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const getCellValue = (tr, idx) => Object.values(tr)[idx];
 
-    const comparer = (idx, asc) => (a, b) => ((v1, v2) => {
-            return v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
-        }
+    const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
+            v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
     )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
 
     const onInputChange = (e, state) => filterData(e.target.value, state);
@@ -39,31 +38,32 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     const init = async () => {
-/*        let initialState = {
-            data: null,
-            configuredData: null,
-            tableBody: null
-        }
-
-        let validator = {
-            set: function (target, key, value) {
-                //console.log(`The property ${key} has been updated with ${value}`);
-                Reflect.set(target, key, value);
-
-                if (key === 'configuredData') {
-                    target.tableBody.innerHTML = '';
-                    appendRows(target.tableBody, target.configuredData);
+        /*        let initialState = {
+                    data: null,
+                    configuredData: null,
+                    tableBody: null
                 }
 
-                return true;
-            }
-        };
-        let state = new Proxy(initialState, validator);*/
+                let validator = {
+                    set: function (target, key, value) {
+                        //console.log(`The property ${key} has been updated with ${value}`);
+                        Reflect.set(target, key, value);
+
+                        if (key === 'configuredData') {
+                            target.tableBody.innerHTML = '';
+                            appendRows(target.tableBody, target.configuredData);
+                        }
+
+                        return true;
+                    }
+                };
+                let state = new Proxy(initialState, validator);*/
 
         let state = {
             data: null,
             configuredData: null,
-            tableBody: null
+            tableBody: null,
+            prevInputValLength: 0
         }
 
         let response = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -75,6 +75,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
         document.querySelector("#search").addEventListener('input', (e) => {
             if (e.target.value.length > 2) onInputChange(e, state);
+
+            if ((e.target.value.length - state.prevInputValLength) < 0) {
+                state.configuredData = state.data;
+                //переписать перерисовку так, чтобы она автоматически срабатывала при изменении стейта
+                state.tableBody.innerHTML = '';
+                appendRows(state.tableBody, state.configuredData);
+            }
+
+            state.prevInputValLength = e.target.value.length;
         });
 
         document.querySelectorAll('.col-head').forEach((th, i) => th.addEventListener('click', (() => {
